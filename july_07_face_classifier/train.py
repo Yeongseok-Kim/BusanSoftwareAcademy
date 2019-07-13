49978cc43c46b4bf1cafe7838a0c4a8b58c542bf
Yeongseok-Kim <50166513+Yeongseok-Kim@users.noreply.github.com>
2019-07-13 23:33:29 +0900

    Update train.py

--- july_07_face_classifier/train.py
+++ july_07_face_classifier/train.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from torch.utils.data.dataset import Dataset
 
 import torchvision
 import torchvision.transforms as transforms
@@ -10,26 +11,57 @@ import torchvision.datasets as dsets
 
 from matplotlib import pyplot as plt
 
+from PIL import Image
+
+import os
+
 import random
 
 # 디바이스 식별
 
 device='cuda' if torch.cuda.is_available() else 'cpu'
 
+# 데이터셋 클래스
+
+class CustomDataset(Dataset):
+    def __init__(self,face_data_dir,nonface_data_dir):
+
+        # face_data_dir 아래의 모든 파일 목록인 face_data_list
+
+        self.face_data_list=[face_data_dir+'/'+file_name for file_name in os.listdir(face_data_dir)]
+        
+        # nonface_data_dir 아래의 모든 파일 목록인 nonface_data_list
+        
+        self.nonface_data_list=[nonface_data_dir+'/'+file_name for file_name in os.listdir(nonface_data_dir)]
+        self.transform=transforms.Compose([transforms.Resize((64,64)),transforms.Grayscale(1),transforms.ToTensor()])
+    def __len__(self):
+
+        # 데이터셋 길이 반환
+
+        return len(self.face_data_list+self.nonface_data_list)
+    def __getitem__(self,index):
+        if index<len(self.face_data_list):
+            img=Image.open(self.face_data_list[index])
+            img=self.transform(img)
+            label=torch.ones(1)
+        else:
+            img=Image.open(self.nonface_data_list[index-len(self.face_data_list)])
+            img=self.transform(img)
+            label=torch.zeros(1)
+        
+        # img,label 반환
+
+        return img,label
+        
 # 트레이닝 데이터 로드
 
 batch_size=100
-
-transform=transforms.Compose([
-    transforms.Resize((64,64)),
-    transforms.Grayscale(1),
-    transforms.ToTensor()])
-train_data=dsets.ImageFolder(root='./face_data/train_data',transform=transform)
+train_data=CustomDataset('./face_data/train_data/face','./face_data/train_data/nonface')
 train_set=DataLoader(train_data,batch_size=batch_size,shuffle=True)
 
 # 테스트 데이터 로드
 
-test_data=dsets.ImageFolder(root='./face_data/test_data',transform=transform)
+test_data=CustomDataset('./face_data/test_data/face','./face_data/test_data/nonface')
 test_set=DataLoader(test_data,len(test_data),shuffle=True)
 
 # 트레이닝
